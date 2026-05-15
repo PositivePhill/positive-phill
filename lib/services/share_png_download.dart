5a5c74e9669ee106b80cb0af14ec3c8f7a7f34a6
Phillip Mattern <162395033+PositivePhill@users.noreply.github.com>
2026-05-15 00:53:02 -0700

    fix: harden share export temp files

--- lib/services/share_png_download.dart
+++ lib/services/share_png_download.dart
@@ -1,7 +1,8 @@
 import 'dart:typed_data';
 
-import 'share_png_download_stub.dart'
-    if (dart.library.html) 'share_png_download_web.dart' as platform;
+import 'package:positive_phill/services/share_png_download_stub.dart'
+    if (dart.library.html) 'package:positive_phill/services/share_png_download_web.dart'
+    as platform;
 
 Future<bool> downloadPngBytes(Uint8List bytes, String filename) =>
     platform.downloadPngBytes(bytes, filename);
--- lib/services/share_temp_file_io.dart
+++ lib/services/share_temp_file_io.dart
@@ -1,15 +1,17 @@
 import 'dart:io';
-import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<String?> writeSharePngBytes(Uint8List bytes) async {
   try {
     final dir = await getTemporaryDirectory();
-    final path = '${dir.path}/positive_phill_share.png';
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    final path = '${dir.path}/positive_phill_share_$timestamp.png';
     await File(path).writeAsBytes(bytes, flush: true);
     return path;
-  } catch (_) {
+  } catch (e, st) {
+    debugPrint('writeSharePngBytes failed: $e\n$st');
     return null;
   }
 }
