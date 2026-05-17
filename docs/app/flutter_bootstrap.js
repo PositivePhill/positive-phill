a8fe16ddf3b03a9291f5fe523c289f1cce9927ee
Phillip Mattern <162395033+PositivePhill@users.noreply.github.com>
2026-05-16 17:41:49 -0700

    deploy: update v1.4.2 video storage hotfix

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2875716336"
+    serviceWorkerVersion: "3998329429"
   }
 });
